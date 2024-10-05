a56466110f297d1fbe05f43d18c6ea7490483760
ThiccThighLover02 <140717531+ThiccThighLover02@users.noreply.github.com>
2024-10-05 16:46:59 +0800

    frontend/HomePage: Added white div to hold signup and login buttons

--- app/Views/home_page/HomePage.php
+++ app/Views/home_page/HomePage.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url() ?>css/styles.css">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        include('Header.php');
+    include('Header.php');
     ?>
-    <img class="w-full p-0 blur-sm" src="<?= base_url() ?>images/munisipyobckgrnd.jpg" alt="">
+    <div class="w-fit grid place-items-center relative">
+        <img class="w-full p-0 blur-sm" src="<?= base_url() ?>images/munisipyobckgrnd.jpg" alt="">
+        <!-- place div absolute and in center of parent -->
+        <div class="absolute min-w-[35rem] h-72 bg-background">
+            
+        </div>
+    </div>
 </body>
+
 </html>
