a0ad63c6f50489795ef21ded29df185bfa38d2ac
ThiccThighLover02 <140717531+ThiccThighLover02@users.noreply.github.com>
2024-10-06 19:04:11 +0800

    frontend/HomePage: Finished div and added signup and login functions

--- app/Views/home_page/HomePage.php
+++ app/Views/home_page/HomePage.php
@@ -15,8 +15,12 @@
     <div class="w-fit grid place-items-center relative">
         <img class="w-full p-0 blur-sm" src="<?= base_url() ?>images/munisipyobckgrnd.jpg" alt="">
         <!-- place div absolute and in center of parent -->
-        <div class="absolute min-w-[35rem] h-72 bg-background">
-            
+        <div class="absolute grid justify-center w-full pt-11 max-w-[550px] rounded-lg h-72 bg-background">
+            <h1 class="text-3xl text-center font-bold tracking-widest">Make Registration EASY!</h1>
+            <div class="flex w-96 justify-between">
+                <a href="<?= base_url() ?>auth/signup" class="grid w-40 h-12 bg-secondary lg:text-2xl text-text place-items-center">Signup</a>
+                <a href="<?= base_url() ?>auth/login" class="grid w-40 h-12 bg-primary lg:text-2xl text-background place-items-center">Login</a>
+            </div>
         </div>
     </div>
 </body>
