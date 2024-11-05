e1e43ecac4ab28cb9af44f1a34bf1bc152bbfca5
ThiccThighLover02 <140717531+ThiccThighLover02@users.noreply.github.com>
2024-11-05 06:12:34 -0700

    frontend/Signup: Completed Guardian info tab for signup

---
+++ app/Views/auth/components/GuardianName.php
@@ -0,0 +1,38 @@
+<div class="flex flex-col gap-2">
+    <h2 class="text-xl font-semibold">Guardian Information:</h2>
+    <div class="flex justify-between">
+        <div>
+            <h2 class="text-lg">First Name:</h2>
+            <input name="guard_first" class="lg:pl-1 lg:min-w-[130px] lg:max-w-[135px] lg:min-h-[23px] lg:max-h-[25px] outline outline-1" placeholder="First Name" type="text">
+        </div>
+        <div>
+            <h2 class="text-lg">Middle Name:</h2>
+            <input name="guard_mid" class="lg:pl-1 lg:min-w-[130px] lg:max-w-[135px] lg:min-h-[23px] lg:max-h-[25px] outline outline-1" placeholder="Middle Name" type="text">
+        </div>
+        <div>
+            <h2 class="text-lg">Last Name:</h2>
+            <input name="guard_last" class="lg:pl-1 lg:min-w-[130px] lg:max-w-[135px] lg:min-h-[23px] lg:max-h-[25px] outline outline-1" placeholder="Last Name" type="text">
+        </div>
+        <div>
+            <h2 class="text-lg">Suffix:</h2>
+            <select class="bg-white lg:pl-1 lg:min-w-[130px] lg:max-w-[135px] lg:min-h-[23px] lg:max-h-[25px] outline outline-1" name="guard_extension" id="extension">
+                <option class="" value="" hidden>Extension</option>
+                <option value="">None</option>
+                <option value="">Jr</option>
+                <option value="">Sr</option>
+            </select>
+        </div>
+    </div>
+
+    <h2 class="text-xl font-semibold">Contact Information:</h2>
+    <div class="flex justify-between">
+        <div>
+            <h2 class="text-lg">Contact Number:</h2>
+            <input name="contact_no" class="lg:pl-1 lg:min-w-[260px] lg:max-w-[255px] lg:min-h-[23px] lg:max-h-[25px] outline outline-1" placeholder="+63 *** *** ****" type="text">
+        </div>
+        <div>
+            <h2 class="text-lg">Email:</h2>
+            <input name="user_email" class="lg:pl-1 lg:min-w-[260px] lg:max-w-[255px] lg:min-h-[23px] lg:max-h-[25px] outline outline-1" placeholder="Email" type="email">
+        </div>
+    </div>
+</div>
---
+++ app/Views/auth/components/Images.php
@@ -0,0 +1,7 @@
+<div>
+    <div class="flex justify-between">
+        <input type="file" name="id_pic" id="">
+        <input type="file" name="birth_certificate" id="">
+        <input type="file" name="barangay_certificate" id="">
+    </div>
+</div>
